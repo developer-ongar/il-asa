64e2a6b6a6e686740dd11a1d88e3f4c34c5878cd
Ongar.dev <91256870+developer-ongar@users.noreply.github.com>
2023-10-28 23:54:13 +0600

    Create KomekIL_labApp.swift

---
+++ iOS/KomekIL_labApp.swift
@@ -0,0 +1,17 @@
+//
+//  KomekIL_labApp.swift
+// 
+//
+//  Created by Администратор on 13/10/2023.
+//
+
+import SwiftUI
+
+@main
+struct KomekIL_labApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
